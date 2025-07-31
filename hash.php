0642c7bc0ba7aa8798a1a3f2a7893c42cbfd1972
yumikohonma <yumikohonma0414@gmail.com>
2025-08-01 05:12:35 +0900

    ログイン情報更新

--- hash.php
+++ hash.php
@@ -1,2 +1,9 @@
 <?php
-echo password_hash("0616", PASSWORD_DEFAULT);
+
+$plainPassword = "rinrin0616";
+
+$hashedPassword = password_hash($plainPassword, PASSWORD_DEFAULT);
+
+echo "ハッシュ化されたパスワード：<br>";
+echo "<textarea rows='3' cols='80' readonly>$hashedPassword</textarea>";
+?>
